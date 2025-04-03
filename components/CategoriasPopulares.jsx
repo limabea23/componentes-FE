b219b36d037c44cd15e8b72f6076c9a0914b4c69
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-04-03 09:03:34 -0300

    Update CategoriasPopulares.jsx

---
+++ components/CategoriasPopulares.jsx
@@ -0,0 +1,25 @@
+import styles from "../styles/Categorias.module.css";
+import CardCategoria from "./CardCategoria";
+
+export default function CategoriasPopulares() {
+    const categorias = [
+        { nome: "Tradicionais", icone: "🍦" },
+    { nome: "Picolés", icone: "🍧" },
+    { nome: "Milkshakes", icone: "🥤" },
+    { nome: "Açaí", icone: "🍇" },
+    { nome: "Vegano", icone: "🌱" },
+    { nome: "Especialidades", icone: "🎂" },
+    { nome: "Novidades", icone: "✨" },
+];
+
+    return (
+        <div className={styles.categorias}>
+            <h2 className={styles.categoriasTitulo}>CATEGORIAS POPULARES</h2>
+            <div className={styles.categoriasCards}>
+            {categorias.map((categoria, index) => (
+                <CardCategoria key={index} icone={categoria.icone} nome={categoria.nome} />
+            ))}
+            </div>
+        </div>
+    );
+}

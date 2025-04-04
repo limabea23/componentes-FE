0f93887b5b9cd7ff66857afe33983a6e8ce75156
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-04-03 23:05:57 -0300

    refactor: order

--- app/gelateria/page.jsx
+++ app/gelateria/page.jsx
@@ -1,10 +1,12 @@
 "use client";
 
 import styles from "../../styles/Home.module.css";
-import Header from "../../components/Header";
-import CategoriasPopulares from "../../components/CategoriasPopulares";
-import Formulario from "../../components/Formulario";
 import Banner from "../../components/Banner";
+import Header from "../../components/Header";
+import Destaques from "../../components/Destaques";
+import CategoriasPopulares from "../../components/CategoriasPopulares";
+import Novidades from "../../components/Novidades";
+import Formulario from "../../components/Formulario";
 import Footer from "../../components/Footer";
 
 export default function Home() {
@@ -12,39 +14,11 @@ export default function Home() {
         <div className={styles.container}>
             <Header />
             <Banner />
-
-    const gelatos = [
-        { imagem: "/gelato di sogno.png", sabor: "Cioccolato", chefe: "por Enrico Fontana", nota: "/5stars.png", categorias: "Tradicionais", categorias: "Especialidades", preco: "R$14.99"},
-        { imagem: "/gelato di sogno.png", sabor: "Stracciatella", chefe: "por Enrico Fontana", categorias: "Tradicionais", categorias: "Especialidades", preco: "R$14.99"},
-    ];
-
-    export default function Destaques () {
-    return (
-        
-
-        
-            //parei aqui
-                <img src={gelato.imagem} alt="gelato di sogno" className={styles.imagem} />
-                <p className={styles.sabor}>{gelato.sabor}</p>
-                <p className={styles.chefe}>{gelato.chefe}</p>
-                <img src={gelato.nota} alt="cinco estrelas" className={styles.nota} />
-                <p className={styles.categorias}>{gelato.categoria}</p>
-                <p className={styles.categorias}>{gelato.categoria}</p>
-                <h2 className={styles.preco}>{gelato.preco}</h2>
-
-                <div className={styles.icons}>
-                <p className={styles.categorias}>🛒</p>
-                <p className={styles.categorias}>👤</p>
-                </div>
-            )
-        </div>
-    )
-}
-
-        <CategoriasPopulares />
-
-        <Formulario />
-        <Footer />
+            <Destaques />
+            <CategoriasPopulares />
+            <Novidades />
+            <Formulario />
+            <Footer />
         </div>
     );
 }
